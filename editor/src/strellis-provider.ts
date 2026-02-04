a367537eee3577dca8d61d0b1fa077df5f0026ad
Defffy <d@defffy.com>
2026-02-04 10:42:48 -1000

    feat: add strellis-provider component with initial implementation

---
+++ editor/src/strellis-provider.ts
@@ -0,0 +1,34 @@
+import { LitElement, unsafeCSS, html } from 'lit'
+import { customElement} from 'lit/decorators.js'
+import styles from './strellis-provider.scss?inline'
+
+/**
+ * A provider component for Strellis editor.
+ */
+@customElement('strellis-provider')
+export class StrellisProvider extends LitElement {
+
+  static styles = unsafeCSS(styles);
+
+  render() {
+    return html`
+      <div class="container">
+        <div class="container__top>
+          <slot name="controls"></slot>
+        </div>
+        <div class="container__editor">
+          <slot name="sidebar"></slot>
+        </div>
+        <div class="container__bottom>
+          <slot name="console"></slot>
+        </div>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'strellis-provider': StrellisProvider 
+  }
+}
