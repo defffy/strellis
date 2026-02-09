af864ffb9c8f9e321c70c23b8b1b78f6c33278fb
Defffy <d@defffy.com>
2026-02-08 22:43:19 -1000

    feat: enhance strellis-display-panel to update iframe content dynamically on 'run-code' event

--- editor/src/components/strellis-display-panel/strellis-display-panel.ts
+++ editor/src/components/strellis-display-panel/strellis-display-panel.ts
@@ -1,5 +1,5 @@
 import { LitElement, unsafeCSS, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
 import styles from './strellis-display-panel.scss?inline'
 
 /**
@@ -10,13 +10,34 @@ export class StrellisDisplayPanel extends LitElement {
 
    static styles = unsafeCSS(styles);
 
+   @property({ type: String, reflect: true })
+   srcDoc!: string;
+
+
+
+   connectedCallback(): void {
+      super.connectedCallback()
+      
+      // Listen for 'run-code' event to update the iframe content
+      window.addEventListener('run-code', (event: Event) => {
+         const iframe = this.shadowRoot?.getElementById('display') as HTMLIFrameElement
+         const customEvent = event as CustomEvent
+
+         console.log(customEvent.detail)
+
+         if (iframe) {
+            this.srcDoc = customEvent.detail.html
+         }
+      })
+   }
+
    render() {
       return html`
       <div class="container">
          <iframe 
          id="display"
-         srcdoc="..." 
-         allow="midi" 
+         srcdoc=${this.srcDoc} 
+         allow="midi; accelerometer; gyroscope; magnetometer" 
          sandbox="allow-scripts">
          </iframe>
       </div>
