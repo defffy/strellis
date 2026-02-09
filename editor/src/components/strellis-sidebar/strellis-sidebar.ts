c8110943279a95c0d57f3f30207025f37e669974
Defffy <d@defffy.com>
2026-02-08 17:52:27 -1000

    feat: enhance file creation process in sidebar with unique filename handling and modal integration

--- editor/src/components/strellis-sidebar/strellis-sidebar.ts
+++ editor/src/components/strellis-sidebar/strellis-sidebar.ts
@@ -1,4 +1,4 @@
-import { LitElement, unsafeCSS, html } from 'lit'
+import { LitElement, unsafeCSS, html, nothing } from 'lit'
 import { customElement, state } from 'lit/decorators.js'
 import styles from './strellis-sidebar.scss?inline'
 
@@ -16,7 +16,7 @@ export class StrellisSidebar extends LitElement {
    files!: string[]
 
    @state()
-   openFileModal: boolean = false
+   openNewFileModal: boolean = false
 
    firstUpdated() {
       this._syncFiles()
@@ -29,31 +29,73 @@ export class StrellisSidebar extends LitElement {
       this.editors = document.querySelectorAll('strellis-editor')
 
       this.files = Array.from(this.editors).map(editor => editor.getAttribute('filename') || '')
-
-      console.log('Files in sidebar:', this.files)
    }
 
    /**
    * Handle click on a file in the sidebar. This should focus the corresponding editor and bring it to the front if it's not already focused.
    */
    _handleFileClick(filename: string) {
-      // Get the editor associated with the clicked file
-      const fileEditor = Array.from(this.editors).find(editor => editor.getAttribute('filename') === filename)
-
-      if (fileEditor) {
-      }
+      this.editors.forEach(editor => {
+         if (editor.getAttribute('filename') === filename) {
+            editor.setAttribute('selected', 'true')
+         } else {
+            editor.setAttribute('selected', 'false')
+         }
+      });
    }
 
    /**
     * Create a new file and open it in a new editor. This should also update the sidebar to include the new file as well.
    */
-   _createFile() { }
+   _createFile(e) {
+      e.preventDefault()
+      // Close the new file modal
+      this.openNewFileModal = false;
+
+      const formData = new FormData(e.target)
+
+      const fileNameData = formData.get('filename') as string
+
+      let fileName = fileNameData.split('.')[0];
+      // If the user didn't provide an extension, default to .js
+      const extension = fileNameData.split('.')[1] || 'js';
+
+      // If there is already a file with the same name and extension, append a number to the end of the file name
+      if (this.files.includes(`${fileName}.${extension}`)) {
+         let counter = 1;
+         let newFileName = `${fileName}-${counter}.${extension}`;
+         while (this.files.includes(newFileName)) {
+            counter++;
+            newFileName = `${fileName}-${counter}.${extension}`;
+         }
+         fileName = `${fileName}-${counter}`;
+      }
+
+      // Create new editor element and append it to the DOM
+      const newEditor = document.createElement('strellis-editor')
+      newEditor.setAttribute('filename', `${fileName}.${extension}`)
+      newEditor.setAttribute('slot', 'editor');
+
+      document.querySelector('strellis-provider')?.appendChild(newEditor)
+
+      this._syncFiles();
+
+      // Ensure the created file is focused
+      this._handleFileClick(`${fileName}.${extension}`);
+
+   }
 
    render() {
       return html`
        <div class="sidebar">
          <div class="sidebar__controls">
             <button class="sidebar__controls__button" @click=${() => this.openNewFileModal = true}>New File</button>  
+            ${this.openNewFileModal ? html`
+               <form @submit=${this._createFile}>
+                  <input name="filename" type="text" placeholder="Enter file name" required />
+                  <button type="submit">Create file</button>
+               </form>
+            ` : nothing} 
          </div>
          <div class="sidebar__files">
             ${this.files?.map(file => html`
