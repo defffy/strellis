a7b0347d94a8bb7ac19ec7a1b50eb8877796e6c8
Defffy <d@defffy.com>
2026-02-10 00:46:27 -1000

    fix: update GitHub Actions workflow for deployment and adjust Vite config base path

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  base: 'strellis',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
