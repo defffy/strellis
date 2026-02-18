4efc84b93feb191e62afdbf26291997ffa02dd58
Defffy <d@defffy.com>
2026-02-17 23:38:51 -1000

    feat: refactor event dispatching to use CustomEvent for STRUDEL_EVENT

--- templates/index/strudel.js
+++ templates/index/strudel.js
@@ -1,16 +1,11 @@
 // Send to iframe context
-const sendToIframe = (event) => {
-  const iframe = document.querySelector("iframe");
-
-  if(iframe) {
-    iframe.contentWindow.postMessage({type: 'STRUDEL_EVENT', val: event}, "*");
-  }
+const dispatchStrudelEvent = (event) => {
+  window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', {detail: event})); // Dispatch custom event in current window
 };
 
 // Use it in your pattern chain
 note("C3 E3 G3").s("sine")
   .fmap((event) => {
-    sendToIframe(event); // Send message as side effect
+    dispatchStrudelEvent(event); // Send message as side effect
     return event; // Return event unchanged for audio to continue
   })
-  .play();
