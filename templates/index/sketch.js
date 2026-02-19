f467e4b90a0253f57ddd63c241a20aec9a8e2c9e
Defffy <d@defffy.com>
2026-02-19 00:10:58 -1000

    feat: set p5 as default template

--- templates/index/sketch.js
+++ templates/index/sketch.js
@@ -7,6 +7,30 @@ window.addEventListener("message", (event) => {
   }
 });
 
+function setup() {
+   createCanvas(windowWidth, windowHeight);
+}
 
-// Any visualization code can go here, using the strudelData object to access the received data
+function draw(){
+  console.log(strudelData); // Log the received data to verify it's working
+   background("blue");
+   fill(getFillFromNote(strudelData.note));
+    const scale = getScaleFromNote(strudelData.note);
+    
+   const n = noise(frameCount * 0.01);
+   ellipse(windowWidth * n, windowHeight * n, 50 * scale, 50 * scale);
+}
 
+function getScaleFromNote(note) {
+  if(note === "C3") return 1;
+  if(note === "G3") return 2;
+  if(note === "E3") return 0.5;
+   return 1;
+}
+
+function getFillFromNote(note) {
+  if(note === "C3") return color(255, 0, 0);
+  if(note === "G3") return color(0, 255, 0);
+  if(note === "E3") return color(0, 0, 255);
+   return color(255);
+}
--- templates/index/strudel.js
+++ templates/index/strudel.js
@@ -1,11 +1,11 @@
 // Send to iframe context
-const dispatchStrudelEvent = (event) => {
+const sendToIframe = (event) => {
   window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', {detail: event})); // Dispatch custom event in current window
 };
 
 // Use it in your pattern chain
 note("C3 E3 G3").s("sine")
   .fmap((event) => {
-    dispatchStrudelEvent(event); // Send message as side effect
+    sendToIframe(event); // Send message as side effect
     return event; // Return event unchanged for audio to continue
   })
