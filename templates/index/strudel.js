1ee68ad613f9bd3576ed04df2f2036381c403ca4
Defffy <d@defffy.com>
2026-03-27 22:09:33 -1000

    feat: refactor event handling to use 'e' function for Strudel_B and Strudel_M

--- templates/index/strudel.js
+++ templates/index/strudel.js
@@ -1,20 +1,31 @@
-// Send to iframe context
-const sendToIframe = (event, eventName) => {
-  window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', {detail: {event, eventName}})); // Dispatch custom event in current window
-};
+const e = (value, eventName) => {
 
+  const msg = {
+    type: "STRUDEL_EVENT",
+    val: value,
+    eventName: eventName,
+  };
+  window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', { detail: msg }));
 
+}
 
 stack(
-s("bd? hh sd? cp").gain(0.5).lpf(1000),
-s("<bd bd>*8").lpf(500).gain(0.5).fmap((event) => {
-    sendToIframe(event, 'STRUDEL_B'); // Send message as side effect
-    return event; // Return event unchanged for audio to continue
+  s("bd? hh sd? cp")
+    .gain(0.5)
+    .lpf(1000),
+  s("bd? hh sd? cp").log(value => {
+    e(value, 'STRUDEL_B')
+    return 'EMIT STRUDEL_B'
   }),
-s("<hh hh hh hh>*4").gain(0.9).lpf(800),
-n("<1 4 3 2>*16").scale("a:minor:pentatonic").s("sawtooth").gain(0.25).vib("<.5 1 2 4 8 16>").fmap((event) => {
-    sendToIframe(event, 'STRUDEL_M'); // Send message as side effect
-    return event; // Return event unchanged for audio to continue
-  }),
-note("<c a g b2>").s('supersaw').tremolo(2)
-)
+  n("<1 4 3 2>*16")
+    .scale("a:minor:pentatonic")
+    .s("sawtooth")
+    .gain(0.25),
+  n("<1 4 3 2>*16")
+    .scale("a:minor:pentatonic")
+    .s("sawtooth")
+    .log(value => {
+      e(value, 'STRUDEL_M')
+      return 'EMIT STRUDEL_M'
+    })
+);
