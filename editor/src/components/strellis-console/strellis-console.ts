e8e40aff86b6e37e2e797db1f53f167bd986b049
Defffy <d@defffy.com>
2026-04-03 00:13:53 -1000

    style: standardize quotes and improve code formatting in StrellisConsole

--- editor/src/components/strellis-console/strellis-console.ts
+++ editor/src/components/strellis-console/strellis-console.ts
@@ -1,51 +1,47 @@
-import { LitElement, unsafeCSS, html } from 'lit'
-import { customElement, query} from 'lit/decorators.js'
-import styles from './strellis-console.scss?inline'
+import { LitElement, unsafeCSS, html } from "lit";
+import { customElement, query } from "lit/decorators.js";
+import styles from "./strellis-console.scss?inline";
 
 /**
- * The console component for Strellis editor. 
+ * The console component for Strellis editor.
  */
-@customElement('strellis-console')
+@customElement("strellis-console")
 export class StrellisConsole extends LitElement {
-
   static styles = unsafeCSS(styles);
 
-   @query('.container')
-   container!: HTMLDivElement;
+  @query(".container")
+  container!: HTMLDivElement;
 
-   firstUpdated() {
-      // Listen for 'iframe-log' events to display logs in the console
-      window.addEventListener('message', (event: MessageEvent) => {
-         const isAtBottom = this.container.scrollHeight - this.container.clientHeight <= this.container.scrollTop + 5;
+  firstUpdated() {
+    // Listen for 'iframe-log' events to display logs in the console
+    window.addEventListener("message", (event: MessageEvent) => {
+      const isAtBottom =
+        this.container.scrollHeight - this.container.clientHeight <=
+        this.container.scrollTop + 5;
 
-         const {data} = event
+      const { data } = event;
 
-         if(data.type !== 'SANDBOX_LOG') return;
+      if (data.type !== "SANDBOX_LOG") return;
 
-         // Create a new log entry element and append it to the console
-         const logEntry = document.createElement('div')
-         logEntry.className = 'console__log-entry'
-         logEntry.textContent = data.content; 
-         this.shadowRoot?.querySelector('.container')?.appendChild(logEntry)
+      // Create a new log entry element and append it to the console
+      const logEntry = document.createElement("div");
+      logEntry.className = "console__log-entry";
+      logEntry.textContent = data.content;
+      this.shadowRoot?.querySelector(".container")?.appendChild(logEntry);
 
-
-
-         if (isAtBottom) {
+      if (isAtBottom) {
         this.container.scrollTop = this.container.scrollHeight;
-    }
-      })
-   }
+      }
+    });
+  }
 
   render() {
-    return html`
-      <div class="container">
-      </div>
-    `
+    return html` <div class="container"></div> `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'strellis-console': StrellisConsole 
+    "strellis-console": StrellisConsole;
   }
 }
