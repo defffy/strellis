bb4dd10dbf75b5910f13c0d8e40709cc03852bc0
Defffy <d@defffy.com>
2026-04-11 23:27:39 -1000

    refactor: enhance sample calls and organize stack function in strudel.js

--- templates/index/strudel.js
+++ templates/index/strudel.js
@@ -15,5 +15,13 @@ Pattern.prototype.emit = function (eventName) {
 
 samples('github:switchangel/pad')
 samples('github:tidalcycles/dirt-samples')
+samples('shabda/speech/en-US/m:this,set,is,done,yes,ah')
 
-s("[bd]*4").gain(0.5).lpf(300).emit('STRUDEL_B');
+
+stack(
+ s("swpad:10").scrub("{0.1!2 .25@3 0.7!2 <0.8:1.5>}%8").emit('M'), 
+  s("[bd]*4").gain(0.5).lpf(300).emit('B')
+)
+
+// Finish
+// s("this set is done yes ah").slow(1.5)
