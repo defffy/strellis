5f95bcdb57e064b09477d89067593333ec3f944d
Defffy <d@defffy.com>
2026-04-11 16:02:59 -1000

    refactor: add emit method to strudel

--- templates/index/strudel.js
+++ templates/index/strudel.js
@@ -1,31 +1,16 @@
-const e = (value, eventName) => {
+Pattern.prototype.emit = function (eventName) {
+  return this.log(function (hap) {
+    window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', {
+      detail: {
+        type: 'STRUDEL_EVENT',
+        val: hap.value,
+        eventName
+      }
+    }));
 
-  const msg = {
-    type: "STRUDEL_EVENT",
-    val: value,
-    eventName: eventName,
-  };
-  window.dispatchEvent(new CustomEvent('STRUDEL_EVENT', { detail: msg }));
 
+    return 'Emitting: ' + eventName;
+  })
 }
 
-stack(
-  s("bd? hh sd? cp")
-    .gain(0.5)
-    .lpf(1000),
-  s("bd? hh sd? cp").log(value => {
-    e(value, 'STRUDEL_B')
-    return 'EMIT STRUDEL_B'
-  }),
-  n("<1 4 3 2>*16")
-    .scale("a:minor:pentatonic")
-    .s("sawtooth")
-    .gain(0.25),
-  n("<1 4 3 2>*16")
-    .scale("a:minor:pentatonic")
-    .s("sawtooth")
-    .log(value => {
-      e(value, 'STRUDEL_M')
-      return 'EMIT STRUDEL_M'
-    })
-);
+s("[bd]*4").gain(0.5).lpf(300).emit('STRUDEL_B');
